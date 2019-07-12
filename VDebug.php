ff6f710e1ee183307328edbc49bfda71a2ca8823
Alexandr <mostali@mail.ru>
2019-07-13 03:56:30 +0600

    update ref

--- VDebug.php
+++ VDebug.php
@@ -1,5 +1,24 @@
 <?php
+class VUtils {
 
+    static function wo($fn, $obj, $mkdir = false) {
+        if ($mkdir)
+            @mkdir(dirname($fn), 0755, true);
+
+        $string_data = serialize($obj);
+        file_put_contents($fn, $string_data);
+    }
+
+    static function ro($fn) {
+        $string_data = file_get_contents($fn);
+        return unserialize($string_data);
+    }
+
+    static function startsWith($haystack, $start) {
+        return $start === "" || strrpos($haystack, $start, -strlen($haystack)) !== false;
+    }
+
+}
 class VDebug {
 
     public static $path = 'vdebug';
@@ -11,15 +30,15 @@ class VDebug {
     static function saveDefVars($arr, $first) {
         $keys = array_keys($arr);
         $pfx = $first ? '$' : '';
-        UFS::wo("mdb/" . $pfx . "vars.arr", $keys, true);
+        VUtils::wo("mdb/" . $pfx . "vars.arr", $keys, true);
         foreach ($keys as $key)
-            UFS::wo("mdb/" . $pfx . "$key.arr", $arr[$key], true);
+            VUtils::wo("mdb/" . $pfx . "$key.arr", $arr[$key], true);
         if ($first) {
             ob_start();
             phpinfo();
             $size = ob_get_contents();
             $info = ob_end_clean();
-            UFS::wo("mdb/" . $pfx . "_PHPINFO.arr", $size, true);
+            VUtils::wo("mdb/" . $pfx . "_PHPINFO.arr", $size, true);
         }
     }
 
@@ -31,10 +50,6 @@ class VDebug {
     }
 
     static function init($get_defined_vars, $include_view = true) {
-        require_once $_SERVER['DOCUMENT_ROOT'] . '/frontend/classes/utils/U.php';
-        require_once $_SERVER['DOCUMENT_ROOT'] . '/frontend/classes/utils/ULog.php';
-        require_once $_SERVER['DOCUMENT_ROOT'] . '/frontend/classes/utils/US.php';
-        require_once $_SERVER['DOCUMENT_ROOT'] . '/frontend/classes/utils/UFS.php';
         VDebug::handle(); //api
         VDebug::clearVars();
         VDebug::saveDefVars($get_defined_vars, true);
@@ -47,11 +62,9 @@ class VDebug {
     }
 
     static function handle() {
-//        if (!self::$isEnbale)
-//            return;
         $path = $_SERVER["REQUEST_URI"];
         $path = substr($path, 1);
-        $start = US::startsWith($path, self::$path);
+        $start = VUtils::startsWith($path, self::$path);
         if (!$start)
             return;
 
@@ -65,15 +78,13 @@ class VDebug {
         else {
             try {
                 $fp = "mdb/$name.arr";
-//                include($fp);
-                $v = @UFS::ro($fp);
+                $v = @VUtils::ro($fp);
                 if ($v === false)
                     throw new Exception('read object');
                 echo json_encode($v);
             } catch (\Exception $e) {
                 echo $e->getMessage();
                 http_response_code(412);
-//                echo "var not found[$name]";
             }
         }
         exit();
@@ -82,8 +93,7 @@ class VDebug {
     static function handle_ALL() {
         try {
             $fp = 'mdb/vars.arr';
-//                include($fp);
-            $v = @UFS::ro($fp);
+            $v = @VUtils::ro($fp);
             if ($v === false)
                 throw new Exception('read object');
             echo json_encode($v);
